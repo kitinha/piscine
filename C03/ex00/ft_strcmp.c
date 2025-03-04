efc02f33e3d8e5ae43067119605e5b3adc727cac
Ines Sofia Guille Marques Baptista Silva <inguille@c1r1s10.42lisboa.com>
2025-03-04 20:41:31 +0000

    c03

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: inguille <inguille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:55:44 by inguille          #+#    #+#             */
-/*   Updated: 2025/03/04 00:01:14 by inguille         ###   ########.fr       */
+/*   Updated: 2025/03/04 20:39:56 by inguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,24 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
 	return (s1[i] - s2[i]);
-	if(s1[i] == s2[i])
-		return (0);
 }
-
+/*
 #include <stdio.h>
 int main(void)
 {
 	char s1[] = "kiti";
-	char s2[] = "kitnha";
+	char s2[] = "kitinha";
 	printf("%d\n", ft_strcmp(s1, s2));
 	char s3[] = "kiti";
 	char s4[] = "kiti";
 	printf("%d\n", ft_strcmp(s3, s4));
 	char s5[] = "kitinha";
-	char s6[] = "kita";
+	char s6[] = "kiti";
 	printf("%d\n", ft_strcmp(s5, s6));
 }
+*/
--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: inguille <inguille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 23:37:38 by inguille          #+#    #+#             */
-/*   Updated: 2025/03/04 00:01:34 by inguille         ###   ########.fr       */
+/*   Updated: 2025/03/04 20:40:06 by inguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,35 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s1[i] < n)
+	while (i < n)
 	{
+		if (s1[i] != s2[i] || s1[i] == '\0')
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
-	}	
-	return (s1[i] - s2[i]);
-	if(s1[i] == s2[i])
-		return (0);
+	}
+	return (0);
 }
-
+/*
 #include <stdio.h>
 int main(void)
 {
-	unsigned int n = 4;
-	char s1[] = "kitik";
+	unsigned int n = 5;
+	char s1[] = "kitis";
 	char s2[] = "kitanhi";
 	printf("%d\n", ft_strncmp(s1, s2, n));
+	char s3[] = "kiti";
+	char s4[] = "kiti";
+	printf("%d\n", ft_strncmp(s3, s4, n));
+	char s5[] = "kitii";
+	char s6[] = "kiti";
+	printf("%d\n", ft_strncmp(s5, s6, n));
+	char s7[] = "kitinha";
+	char s8[] = "kitinha";
+	printf("%d\n", ft_strncmp(s7, s8, n));
+	char s9[] = "kati";
+	char s11[] = "kiti";
+	printf("%d\n", ft_strncmp(s9, s11, n));
 }
+*/
