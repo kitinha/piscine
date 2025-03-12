1afc243697a0630c13f072c994da537a64720276
kitinha <inguille@42kitita.myguest.virtualbox.org>
2025-03-12 01:48:24 +0000

    refeito

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inguille <inguille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 00:55:22 by inguille          #+#    #+#             */
+/*   Updated: 2025/03/12 01:46:50 by inguille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+/*
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", ft_fibonacci(23));   
+}
+*/
