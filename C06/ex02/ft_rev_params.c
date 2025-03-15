cee9b4d1d809a440163915753392c74a9e784c4a
kita <sm4sh200@gmail.com>
2025-03-15 19:27:02 +0000

    c06

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -6,16 +6,29 @@
 /*   By: inguille <inguille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:59:40 by inguille          #+#    #+#             */
-/*   Updated: 2025/03/13 18:00:01 by inguille         ###   ########.fr       */
+/*   Updated: 2025/03/15 17:47:36 by inguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+
 int	main(int argc, char **argv)
 {
 	int	i;
-	
+	int y;
 
-
+	i = argc - 1;
+	while (i >= 1)
+	{
+		y = 0;
+		while (argv[i][y] != '\0')
+		{
+			write(1, &argv[i][y], 1);
+			y++;
+		}
+		write(1, "\n", 1);
+		i--;
+	}
+	return (0);
 }
