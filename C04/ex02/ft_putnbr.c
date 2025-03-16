fc907990deba07ea09b21392bf7ebd10614404fa
kitinha <inguille@42kitita.myguest.virtualbox.org>
2025-03-16 13:22:28 +0000

    gata

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: inguille <inguille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 21:12:50 by inguille          #+#    #+#             */
-/*   Updated: 2025/03/09 00:17:20 by inguille         ###   ########.fr       */
+/*   Updated: 2025/03/16 13:21:23 by inguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,26 @@ void	putchar(char y)
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	if (nb == -2147483648)
 	{
-		putchar('-');
-		nb = -nb;
+		write (1, "-2147483648", 11);
 	}
-	if (nb >= 10)
+	else
 	{
-		ft_putnbr (nb / 10);
-		ft_putnbr (nb % 10);
+		
+		if (nb < 0)
+		{
+			putchar('-');
+			nb = -nb;
+		}
+		if (nb >= 10)
+		{
+			ft_putnbr (nb / 10);
+			ft_putnbr (nb % 10);
+		}
+		if (nb < 10)
+			putchar (nb + '0');
 	}
-	if (nb < 10)
-		putchar (nb + '0');
 }
 /*
 int main()
